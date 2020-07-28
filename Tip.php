e872d6f7fedd511f4302b11a56b7ab44a9f9903d
Nshan Vardanyan <42932424+Nshan0102@users.noreply.github.com>
2020-07-28 17:01:25 +0400

    Create Tip

---
+++ Tip.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * Class Tip
+ */
+class Tip
+{
+    /**
+     * @param $dateOne
+     * @param $dateTwo
+     * @return array
+     */
+    public function getDifferenceBetweenDates($dateOne, $dateTwo)
+    {
+        $date1 = strtotime($dateOne);
+        $date2 = strtotime($dateTwo);
+        $diff = $date1 > $date2 ? abs($date1 - $date2) : abs($date2 - $date1);
+        $years = floor($diff / (365 * 60 * 60 * 24));
+        $months = floor(($diff - $years * 365 * 60 * 60 * 24)
+            / (30 * 60 * 60 * 24));
+        $days = floor(($diff - $years * 365 * 60 * 60 * 24 -
+                $months * 30 * 60 * 60 * 24) / (60 * 60 * 24));
+        $hours = floor(($diff - $years * 365 * 60 * 60 * 24
+                - $months * 30 * 60 * 60 * 24 - $days * 60 * 60 * 24)
+            / (60 * 60));
+        $minutes = floor(($diff - $years * 365 * 60 * 60 * 24
+                - $months * 30 * 60 * 60 * 24 - $days * 60 * 60 * 24
+                - $hours * 60 * 60) / 60);
+        $seconds = floor(($diff - $years * 365 * 60 * 60 * 24
+            - $months * 30 * 60 * 60 * 24 - $days * 60 * 60 * 24
+            - $hours * 60 * 60 - $minutes * 60));
+        return [
+            'years' => $years,
+            'months' => $months,
+            'days' => $days,
+            'hours' => $hours,
+            'minutes' => $minutes,
+            'seconds' => $seconds
+        ];
+    }
+}
